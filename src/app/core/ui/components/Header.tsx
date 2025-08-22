db659623c08d8be58114d36cf062f86159394f28
pinkth3floyd <prakash@zeptosky.com>
2025-08-22 22:23:38 +0545

    Header fixes

--- src/app/core/ui/components/Header.tsx
+++ src/app/core/ui/components/Header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
               {/* Header Logo */}
               <Link href="/" className="header__logo">
                 {/* <img src="/core/assets/img/logo.svg" alt="CineHUB" /> */}
-                <h2>CineHUB</h2>
+                <h2 className="text-white">CineHUB</h2>
               </Link>
 
               {/* Header Navigation */}
@@ -70,79 +70,6 @@ export default function Header() {
                   </li>
                 ))}
               </ul>
-
-              {/* Header Auth */}
-              <div className="header__auth">
-                {/* Search Form */}
-                <form action="#" className="header__search">
-                  <input 
-                    className="header__search-input" 
-                    type="text" 
-                    placeholder="Search..."
-                  />
-                  <button className="header__search-button" type="button">
-                    <i className="ti ti-search"></i>
-                  </button>
-                  <button className="header__search-close" type="button">
-                    <i className="ti ti-x"></i>
-                  </button>
-                </form>
-
-                {/* Search Toggle Button */}
-                <button 
-                  className="header__search-btn" 
-                  type="button"
-                  onClick={() => setIsSearchVisible(!isSearchVisible)}
-                >
-                  <i className="ti ti-search"></i>
-                </button>
-
-                {/* Language Dropdown */}
-                <div className="header__lang">
-                  <a 
-                    className="header__nav-link" 
-                    href="#" 
-                    role="button" 
-                    data-bs-toggle="dropdown" 
-                    aria-expanded="false"
-                  >
-                    EN <i className="ti ti-chevron-down"></i>
-                  </a>
-
-                  <ul className="dropdown-menu header__dropdown-menu">
-                    {languageItems.map((item, index) => (
-                      <li key={index}>
-                        <Link href={item.href}>{item.label}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-
-                {/* User Profile Dropdown */}
-                {/* <div className="header__profile">
-                  <a 
-                    className="header__sign-in header__sign-in--user" 
-                    href="#" 
-                    role="button" 
-                    data-bs-toggle="dropdown" 
-                    aria-expanded="false"
-                  >
-                    <i className="ti ti-user"></i>
-                    <span>Nickname</span>
-                  </a>
-
-                  <ul className="dropdown-menu dropdown-menu-end header__dropdown-menu header__dropdown-menu--user">
-                    {userMenuItems.map((item, index) => (
-                      <li key={index}>
-                        <Link href={item.href}>
-                          <i className={item.icon}></i>
-                          {item.label}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div> */}
-              </div>
 
               {/* Mobile Menu Button */}
               <button className="header__btn" type="button">
